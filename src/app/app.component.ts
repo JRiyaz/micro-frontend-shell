3efc30a6cbb6f391c19a64dca6fe6efb7f7050fa
Riyaz J <j.riyazu@gmail.com>
2026-04-29 16:10:47 +0530

    Add Configuration for Notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  NotificationToastComponent,
+  NotificationSidenavComponent,
+  NotificationService,
+} from 'ui-shared';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: ` <router-outlet /> `,
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NotificationToastComponent,
+    NotificationSidenavComponent,
+  ],
+  template: `
+    <router-outlet />
+
+    <!-- Global Notifications Layer -->
+    <ui-notification-toast />
+    <ui-notification-sidenav
+      *ngIf="notificationService.sidenavOpen()"
+      (close)="notificationService.sidenavOpen.set(false)"
+    />
+  `,
   styles: [],
 })
 export class AppComponent {
   title = 'shell';
+  notificationService = inject(NotificationService);
 }
